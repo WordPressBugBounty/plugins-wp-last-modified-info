a1f353ef1951b84b26f289686f783ebc29b5a4e2
Sean Murphy <sean@iamseanmurphy.com>
2025-04-28 07:33:05 +0000

    1.9.2

--- wp-last-modified-info/blocks/build/post-template-tag/index.asset.php
+++ wp-last-modified-info/blocks/build/post-template-tag/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => '8ed05c9809a33a9f17be');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-server-side-render'), 'version' => 'd8af43e37f8bbf93a986');
